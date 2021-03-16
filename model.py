90330a233b9f76cabc61f6d4df52bf578d48cc3f
Roger-Mei <rogermei@umich.edu>
2021-03-16 18:51:17 -0400

    Add 3 camera images

--- model.py
+++ model.py
@@ -15,17 +15,31 @@ with open('../../../opt/carnd_p3/data/driving_log.csv') as csvfile:
         
 images = []
 measurements = []
+correction = 0.25
 for line in lines[1:]:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../../../opt/carnd_p3/data/IMG/' + filename
-    image = ndimage.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = '../../../opt/carnd_p3/data/IMG/' + filename
+        image = ndimage.imread(current_path)
+        images.append(image)
+        if i == 0:
+            measurement = float(line[3])
+            measurements.append(measurement)
+        if i == 1:
+            measurement = float(line[3]) - correction
+            measurements.append(measurement)
+        if i == 2:
+            measurement = float(line[3]) + correction
+            measurements.append(measurement)
     
 X_train = np.array(images)
 y_train = np.array(measurements)
+
+################################################################################
+# Data Augmentation
+################################################################################
+
 
 ################################################################################
 # Network Architecture (LeNet)
